ba97b03a5e92b2cc5563412a20b4be04466551eb
Baehyewon <bhjn8991@gmail.com>
2019-09-05 18:51:13 +0900

    drawer 연결

--- app/src/main/java/com/meong/podoandroid/MainActivity.java
+++ app/src/main/java/com/meong/podoandroid/MainActivity.java
@@ -44,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
         txtHospital.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapSearchActivity.class);
+                startActivity(intent);
 
+                finish();
             }
         });
 
