3fb44cc2038be9ceab8377d42a8dc8a79bd6464b
songkyunghee <kyunghee794613@gmail.com>
2019-09-09 21:14:21 +0900

    database helper class create

---
+++ app/src/main/java/com/meong/podoandroid/DatabaseHelper.java
@@ -0,0 +1,37 @@
+package com.meong.podoandroid;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public DatabaseHelper(Context context) {
+        super(context, "feed.db",null,1);
+
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String tableName1 = "feed_leg";
+        String tableName2="feed_calory";
+        String tableName3="feed_wellbing";
+
+        String sql1="create table if not exists "  + tableName1+"(_id integer PRIMARY KEY autoincrement, title text, url text, content text)";
+        String sql2="create table if not exists "  + tableName2+"(_id integer PRIMARY KEY autoincrement, title text, url text, content text)";
+        String sql3="create table if not exists "  + tableName3+"(_id integer PRIMARY KEY autoincrement, title text, url text, content text)";
+
+        sqLiteDatabase.execSQL(sql1);
+        sqLiteDatabase.execSQL(sql2);
+        sqLiteDatabase.execSQL(sql3);
+
+        Log.e("msg","table create");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS feed");
+        onCreate(sqLiteDatabase);
+
+    }
+}
